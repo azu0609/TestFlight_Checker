077474d8f470ffc0e464e6b673e2358d0e76acfb
azu0609 <ipadgoogcloud@gmail.com>
2023-05-28 11:08:16 +0900

    Feat: add cli arguments, code cleanup

--- check.py
+++ check.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-import sys
+import typer
+import time
 
 
 class Checker():
-    def __init__(self, url: str):
+    def __init__(self, amounts: int = typer.Option(1, "-a", "--amount"), interval: int = typer.Option(1, "-i", "--interval"), url: str = typer.Option(None, "-u", "--url")) -> None:
         self.site_recipe = [
             {
                 "name": "osu!",
@@ -13,17 +14,32 @@ class Checker():
                 "body_tag": "a"
             },
         ]
-        for recipe in self.site_recipe:
-            print("RECIPE: Finding recipe")
-            if recipe["name"] in url:
-                print(f"{recipe['name']}: Found recipe. Following recipe...")
-                print(f"{recipe['name']}: Sending request to: {recipe['target_url']}")
-                self.response = requests.get(recipe["target_url"], headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0'})
-                print(f"{recipe['name']}: Fetching testflight link from: {recipe['target_url']}")
-                url = self.fetch_html(recipe["class_name"], recipe["body_tag"])
-                print(f"{recipe['name']}: Done.")
+        if amounts:
+            for recipe in self.site_recipe:
+                print("RECIPE: Finding recipe")
+                if recipe["target_url"] in url:
+                    print(f"{recipe['name']}: Found recipe. Following recipe...")
+                    print(f"{recipe['name']}: Sending request to: {recipe['target_url']}")
+                    self.response = requests.get(recipe["target_url"], headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0'})
+                    print(f"{recipe['name']}: Fetching testflight link from: {recipe['target_url']}")
+                    url = self.fetch_html(recipe["class_name"], recipe["body_tag"])
+                    print(f"{recipe['name']}: Done.")
+                    break
 
-        # Send a GET request to the website
+            for _ in range(amounts):
+                self.http_request(url)
+                time.sleep(interval)
+        else:
+            raise Exception("Arguments not found")
+    
+    def fetch_html(self, class_name, element_type):
+        soup = BeautifulSoup(self.response.content, 'html.parser', from_encoding="utf-8") # Create BeautifulSoup object
+        div_element = soup.find('div', class_=class_name) # Find the <div> element with class 'beta-status'
+        text = div_element.find(element_type).get_text() # Find the text element within the <div> element, and extract the text from the text element
+
+        return text
+    
+    def http_request(self, url):
         self.response = requests.get(url)
         if self.response.status_code == 200:
             status = self.fetch_html("beta-status", "span")
@@ -39,18 +55,6 @@ class Checker():
             print("ERROR: Rate limited by Apple. Try again later")
         else:
             raise Exception(self.response.status_code + ": " + self.response.text)
-    
-    def fetch_html(self, class_name, element_type):
-        soup = BeautifulSoup(self.response.content, 'html.parser', from_encoding="utf-8") # Create BeautifulSoup object
-        div_element = soup.find('div', class_=class_name) # Find the <div> element with class 'beta-status'
-        text_element = div_element.find(element_type) # Find the text element within the <div> element
-        text = text_element.get_text() # Extract the text from the text element
 
-        return text
-    
-
-if len(sys.argv) > 1:
-    url = sys.argv[1:][0]
-    Checker(url)
-else:
-    raise Exception("No arguments found.")
+if __name__ == "__main__":
+    typer.run(Checker)
